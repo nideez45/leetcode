45a72c4633b54972348b8e495294235ec60657bb
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-08-05 11:15:30 +0530

    Time: 44 ms (5.24%), Space: 13.1 MB (81.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     
-    // O(nm) Time and O(n+m) space
-    // use two array of size n amd m to determine whether a patricular row
-    // or column has to be made into 0
+    // O(nm) Time and O(1) space
+    // try to extend the linear space idea
+    // instead of using additional space try to store whether a row or column has to be made 0
+    // in first row and first column itself
     
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> v1(n,0);
-        vector<int> v2(m,0);
+        bool first_row = false;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j] == 0){
-                    v1[i] = 1;
-                    v2[j] = 1;
+                    matrix[0][j] = 0;
+                    if(i == 0) first_row = true;
+                    else matrix[i][0] = 0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            if(v1[i]){
+        for(int i=1;i<n;i++){
+            if(matrix[i][0] == 0){
                 for(int j=0;j<m;j++) matrix[i][j] = 0;
             }
         }
+        
         for(int j=0;j<m;j++){
-            if(v2[j]){
+            if(matrix[0][j] == 0){
                 for(int i=0;i<n;i++) matrix[i][j] = 0;
             }
         }
+        
+        if(first_row) for(int j=0;j<m;j++) matrix[0][j] = 0;
         
     }
 };
