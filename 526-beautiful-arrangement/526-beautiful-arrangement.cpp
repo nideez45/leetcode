4dc3ef6f877e788b59d998ab1b931d6941b9fa08
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-08-12 16:12:50 +0530

    Time: 7 ms (96.72%), Space: 8 MB (26.32%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[15][1<<15];
+    int n;
+    
+    int rec(int level,int mask){
+        if(level == n){
+            return 1;
+        }
+        if(dp[level][mask] != -1) return dp[level][mask];
+        
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(!(mask&(1<<i))){
+                if((level+1)%(i+1) == 0 || (i+1)%(level+1) == 0){
+                    int temp_mask = mask^(1<<i);
+                    ans+=rec(level+1,temp_mask);
+                }
+            }
+        }
+        return dp[level][mask] = ans;
+    }
+    
+    int countArrangement(int _n) {
+        n = _n;
+        memset(dp,-1,sizeof(dp));    
+        return rec(0,0);
+    }
+};
