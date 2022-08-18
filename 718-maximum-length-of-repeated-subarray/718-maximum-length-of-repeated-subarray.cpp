3e26578ad624be256656b538f046d05479fc35d7
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-08-18 12:10:47 +0530

    Time: 93 ms (98.50%), Space: 22.3 MB (60.63%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,40 +1,67 @@
 class Solution {
 public:
+    // binary search on the length
+    // can check if a length is possible with hashing
     
-    // length longest common subarray - O(N^2) with dp
-    
-    int dp[1001][1001];
-    vector<int> nums1;
-    vector<int> nums2;
-    int n,m;
-    
-    int rec(int level1,int level2){
-        if(level1 == n || level2 == m){
-            return 0;
-        }
-        if(dp[level1][level2] != -1){
-            return dp[level1][level2];
-        }
-        int ans;
-        if(nums1[level1] != nums2[level2]) ans = 0;
-        else{
-            ans = 1+rec(level1+1,level2+1);
-        }
-        return dp[level1][level2] = ans;
+    int getHash(int l,int r,vector<int> &hash,vector<int> &pk){
+        int n = hash.size();
+        int mod = 1e9+7;
+        if(l == 0) return hash[r];
+        return (hash[r]-((1LL*hash[l-1]*pk[r-l+1])%mod)+mod)%mod;
     }
     
-    int findLength(vector<int>& _nums1, vector<int>& _nums2) {
-        nums1 = _nums1;
-        nums2 = _nums2;
-        n = nums1.size();
-        m = nums2.size();
-        memset(dp,-1,sizeof(dp));
+    bool check(int len,vector<int>&hash1,vector<int>&pk1,vector<int>&hash2,vector<int>&pk2){
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+        for(int i=len-1;i<hash1.size();i++){
+            // starting at index i-k+1 and ending at index i
+            mp1[getHash(i-len+1,i,hash1,pk1)]++;
+        }
+        for(int i=len-1;i<hash2.size();i++){
+            mp2[getHash(i-len+1,i,hash2,pk2)]++;
+        }
+        for(auto pr:mp1){
+            if(mp2.find(pr.first) != mp2.end()) return true;
+        }
+        return false;
+    }
+    
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int p = 131;
+        int mod = 1e9+7;
+        vector<int> pk1;
+        vector<int> pk2;
+        vector<int> hash1;
+        vector<int> hash2;
+        pk1.resize(nums1.size());
+        hash1.resize(nums1.size());
+        pk2.resize(nums2.size());
+        hash2.resize(nums2.size());
+        hash1[0] = nums1[0]+1;
+        hash2[0] = nums2[0]+1;
+        pk1[0] = pk2[0] = 1;
+        for(int i=1;i<nums1.size();i++){
+            hash1[i] = ((1LL*hash1[i-1]*p)%mod + (nums1[i]+1))%mod;
+            pk1[i] = (1LL*pk1[i-1]*p)%mod;
+        }
+        for(int i=1;i<nums2.size();i++){
+            hash2[i] = ((1LL*hash2[i-1]*p)%mod + (nums2[i]+1))%mod;
+            pk2[i] = (1LL*pk2[i-1]*p)%mod;
+        }
         int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans = max(ans,rec(i,j));
+        int lo = 1;
+        int hi = min(nums1.size(),nums2.size());
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(check(mid,hash1,pk1,hash2,pk2)){
+                ans = mid;
+                lo = mid+1;
+            }
+            else{
+                hi = mid-1;
             }
         }
+      //  cout<<getHash(2,3,hash1,pk1)<<" "<<getHash(0,1,hash2,pk2)<<'\n';
         return ans;
     }
 };
