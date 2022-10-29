ff93246943570906fc2a7cc630f9c16610f77100
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-10-29 07:11:16 +0530

    Time: 205 ms (82.79%), Space: 67.7 MB (75.53%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -7,20 +7,31 @@ public:
     // or the standard prefix sum way
     // prefj-prefi = k
     // 
-     
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    
+    int atmostk(vector<int>&nums,int k){
+        int sm = 0;
+        int cnt = 0;
+        int head = -1;
+        int tail = 0;
         int n = nums.size();
-        map<int,int> mp;
-        mp[0] = 1;
-        int pref = 0;
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            pref += nums[i]%2;
-            if(mp.find(pref-k) != mp.end()){
-                ans+=mp[pref-k];
+        while(tail<n){
+            while(head+1<n && (sm<k || nums[head+1]%2 == 0)){
+                head++;
+                if(nums[head]%2 == 1) sm++;
             }
-            mp[pref]++;
+            cnt += (head-tail+1);
+            if(head>=tail){
+                if(nums[tail]%2) sm--;
+                tail++;
+            }else{
+                tail++;
+                head = tail-1;
+            }
         }
-        return ans;
+        return cnt;
+    }
+    
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atmostk(nums,k) - atmostk(nums,k-1);
     }
 };
