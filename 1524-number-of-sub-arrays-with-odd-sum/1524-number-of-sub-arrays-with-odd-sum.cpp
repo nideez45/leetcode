96c5bfbf461932af8b39a8128c38fc6c79b3774f
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-10-29 07:31:42 +0530

    Time: 388 ms (31.98%), Space: 108.1 MB (36.51%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
     
-    // contribution of each index?
-    // dp1[i] be number of subarray starting at index i and have odd sum
-    // dp2[i] be number of subarray starting at index i and have even sum
+    // can do with no additional extra space ig
     
     const int mod = 1e9+7;
     
     int numOfSubarrays(vector<int>& arr) {
         int n = arr.size();
-        int dp1[n];
-        int dp2[n];
-        for(int i=0;i<n;i++) {
-            dp1[i] = 0;
-            dp2[i] = 0;
-        }
-        dp1[n-1] = arr[n-1]%2;
-        dp2[n-1] = 1 - arr[n-1]%2;
-        for(int i=n-2;i>=0;i--){
-            if(arr[i]%2==0){
-                dp1[i] += (dp1[i+1]);
-                dp1[i]%=mod;
-                dp2[i] += dp2[i+1];
-                dp2[i] +=1;
-                dp2[i]%=mod;
+        int ans = 0;
+        int oc=0;
+        int ec=1;
+        int pref = 0;
+        for(int i=0;i<n;i++){
+            pref+=arr[i]%2;
+            if(pref%2 == 0){
+                ans += oc;
+                ans%=mod;
+                ec++;
             }
             else{
-                dp1[i] += dp2[i+1];
-                dp1[i] +=1;
-                dp1[i]%=mod;
-                dp2[i] += dp1[i+1];
-                dp2[i]%=mod;
+                ans+=ec;
+                ans%=mod;
+                oc++;
             }
         }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            ans+=dp1[i];
-            ans%=mod;
-        }
-        // for(int i=0;i<n;i++){
-        // cout<<dp1[i]<<" "<<dp2[i]<<'\n';
-        // }
         return ans;
     }
 };
