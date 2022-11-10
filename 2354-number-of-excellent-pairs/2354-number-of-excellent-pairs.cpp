0332707ba142ecbd1b74e520157089c98741bddb
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-11-10 19:20:25 +0530

    Time: 1665 ms (5.22%), Space: 151 MB (5.21%) - LeetHub

---
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    // number of set bits in a|b + number of set bits in a&b = number of sets bits in a + number of set bits in b
+    
+    int bc(int num){
+        int cnt = 0;
+        for(int i=0;i<31;i++){
+            if((num&(1<<i))) cnt++;
+        }
+        return cnt;
+    }
+    
+    long long countExcellentPairs(vector<int>& nums, int k) {
+        set<int>st;
+        for(auto val:nums) st.insert(val);
+        vector<int> v;
+        map<int,int> mp;
+        for(auto val:nums) mp[val]++;
+        for(auto val:st) v.push_back(val);
+        vector<int> x;
+        for(auto val:v){
+            x.push_back(bc(val));
+        }
+        sort(x.begin(),x.end());
+        long long ans = 0;
+        int n = x.size();
+        for(int i=0;i<n;i++){
+            auto it = x.begin()+i+1;
+            int idx = lower_bound(it,x.end(),k-x[i])-x.begin();
+            if(idx>=i){
+                ans+= 1LL*(n-idx);
+            }
+        }
+        ans*=2LL;
+        // handles the cases where same element forms pair
+        for(auto val:x){
+            if(2*val>=k) ans++;
+        }
+        
+        return ans;
+    }
+};
