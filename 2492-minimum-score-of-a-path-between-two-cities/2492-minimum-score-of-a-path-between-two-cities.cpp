022094936d2d8c7d423da7b8ef425d75d03cb843
Nideesh N <75631297+nideez45@users.noreply.github.com>
2022-12-04 16:06:41 +0530

    Time: 522 ms (90.48%), Space: 119.9 MB (76.19%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -6,7 +6,7 @@ public:
     
     void dfs(int v){
         visited[v] = 1;
-        for(int child:g[v]){
+        for(int &child:g[v]){
             if(!visited[child]) dfs(child);
         }
     }
@@ -14,12 +14,12 @@ public:
     int minScore(int n, vector<vector<int>>& roads) {
 
         int ans = 1e5;
-        for(auto e:roads){
+        for(auto &e:roads){
             g[e[0]].push_back(e[1]);
             g[e[1]].push_back(e[0]);
         }
         dfs(1);
-        for(auto e:roads){
+        for(auto &e:roads){
             if(visited[e[0]]) ans = min(ans,e[2]);
         }
         return ans;
